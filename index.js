aebcfc181544efa2c8781a6ba1a9dbf1ec84a06c
srinivastalnikar <97269178+srinivastalnikar@users.noreply.github.com>
2022-01-13 23:02:14 -0800

    added query-string module & deleted node_modules

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const mysql = require('mysql');
 const port = 3000;
 const app = express();
-
+const queryString = require('query-string')
 
 const db = mysql.createConnection({
   host: 'bi30tloqqg6aqnsjzk5h-mysql.services.clever-cloud.com',
   user: 'uapdthevueb7t3hg',
-  password: 'zEZ6Q84UgLSHREfJwowg',
+  password: 'zEZ6 Q84UgLSHREfJwowg',
   database: 'bi30tloqqg6aqnsjzk5h'
 });
 
@@ -22,8 +22,8 @@ app.get('/getUsers', (req,res) => {
   let sql = `select * from user`
   db.query(sql, (err, result) => {
     if(err) throw err;
-    res.send('Databse Created');
-    console.log(result);
+    console.log('Current Users Are');
+    res.send(result);
   });
 });
 
