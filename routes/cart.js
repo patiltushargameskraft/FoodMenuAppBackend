313119952ff8f80006b4cdd66d6c25e071f141a5
srinivastalnikar <97269178+srinivastalnikar@users.noreply.github.com>
2022-01-23 20:33:38 -0800

    changed request type of decreaseQuantityInCart

--- routes/cart.js
+++ routes/cart.js
@@ -31,7 +31,7 @@ router.delete('/checkOutCartItems/:userId', (req, res) => {
     setData(res, sql.checkOutCartItems(userId));
 })
 
-router.delete('/decreaseQuantityInCart/:orderId', (req, res) => {
+router.post('/decreaseQuantityInCart/:orderId', (req, res) => {
     const {orderId} = req.params;
     setData(res, sql.decreaseQuantityInCart(orderId));
 })
